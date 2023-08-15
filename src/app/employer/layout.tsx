7018c2f9a35b306d88eb72731b16ca29cbd3e773
Gabriel Fernandes <bielfer18@gmail.com>
2023-08-15 14:31:52 -0300

    feat: if no organization is chosen, choose it

--- src/app/employer/layout.tsx
+++ src/app/employer/layout.tsx
@@ -1,12 +1,31 @@
 import type { FC, ReactNode } from "react";
 import SidebarWrapper from "./sidebar-wrapper";
+import { auth } from "@clerk/nextjs";
+import EmptyState from "~/components/empty-state";
+import { UserGroupIcon } from "@heroicons/react/24/outline";
 
 type Props = {
   children: ReactNode;
 };
 
 const EmployerLayout: FC<Props> = ({ children }) => {
-  return <SidebarWrapper>{children}</SidebarWrapper>;
+  const { orgId } = auth();
+
+  return (
+    <SidebarWrapper>
+      {!orgId ? (
+        <div className="pt-10">
+          <EmptyState
+            icon={UserGroupIcon}
+            title="Nenhuma organização escolhida!"
+            subtitle="Para escolher ou criar um organização basta clicar no botão espaço pessoal à esquerda!"
+          />
+        </div>
+      ) : (
+        children
+      )}
+    </SidebarWrapper>
+  );
 };
 
 export default EmployerLayout;
