bd33c9de4b29ac27e386f3d654f7d95500f76a0a
Gabriel Fernandes <bielfer18@gmail.com>
2023-08-17 09:49:50 -0300

    fix: translate not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,5 @@
-import MyLink from '~/components/my-link';
-import { paths } from '~/constants/paths';
+import MyLink from "~/components/my-link";
+import { paths } from "~/constants/paths";
 
 const Page404 = () => (
   <div className="flex h-screen flex-col bg-white pb-12 pt-16">
@@ -7,21 +7,21 @@ const Page404 = () => (
       <div className="py-16">
         <div className="text-center">
           <p className="text-sm font-semibold uppercase tracking-wide text-primary-600">
-            Error 404
+            Erro 404
           </p>
           <h1 className="mt-2 text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl">
-            Page not found
+            Página não encontrada
           </h1>
           <p className="mt-2 text-base text-gray-500">
-            Sorry, but it was not possible to find the page you were looking
-            for.
+            Desculpa, mas não foi possível encontrar a página que você estava
+            procurando.
           </p>
           <div className="mt-6">
             <MyLink
               href={paths.home}
               className="text-base font-medium text-primary-600 hover:text-primary-500"
             >
-              Home Page<span aria-hidden="true"> &rarr;</span>
+              Página Inicial<span aria-hidden="true"> &rarr;</span>
             </MyLink>
           </div>
         </div>
