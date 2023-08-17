82c51b4bc3d4215a343800fb7d42b45db1e917ef
Gabriel Fernandes <bielfer18@gmail.com>
2023-08-17 09:54:23 -0300

    feat: employer clock ins page

--- src/app/employer/clock-ins/page.tsx
+++ src/app/employer/clock-ins/page.tsx
@@ -1,9 +1,18 @@
 import type { FC } from "react";
+import { type Metadata } from "next";
+import EmployeesList from "./employees-list";
+
+export const metadata: Metadata = {
+  title: "Pontos dos Funcionários | ZipRH",
+  description:
+    "Uma lista completa de todos os seus funcionários, em que você pode acessar os pontos batidos por cada um",
+};
 
 const EmployerClockIns: FC = () => {
   return (
     <>
       <h1>Pontos</h1>
+      <EmployeesList />
     </>
   );
 };
