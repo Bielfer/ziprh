23a67b3683d631a782e7d737b7477cd63da538d2
Gabriel Fernandes <bielfer18@gmail.com>
2023-08-18 17:00:05 -0300

    fix: extend empty state className

--- src/components/empty-state/empty-state.tsx
+++ src/components/empty-state/empty-state.tsx
@@ -1,12 +1,14 @@
-import { type IconType } from '~/types/core';
-import clsx from 'clsx';
-import type { FC, ReactElement } from 'react';
+import { type IconType } from "~/types/core";
+import clsx from "clsx";
+import type { FC, ReactElement } from "react";
+import cn from "~/helpers/cn";
 
 type Props = {
   icon?: IconType;
   title: string;
   subtitle?: string;
   buttonOrLink?: ReactElement;
+  className?: string;
 };
 
 const EmptyState: FC<Props> = ({
@@ -14,12 +16,13 @@ const EmptyState: FC<Props> = ({
   title,
   subtitle,
   buttonOrLink,
+  className,
 }) => (
-  <div className="text-center">
+  <div className={cn("text-center", className)}>
     {Icon && <Icon className="mx-auto h-12 w-12 text-gray-400" />}
     <h3 className="mt-2 text-sm font-medium text-gray-900">{title}</h3>
     {subtitle && (
-      <p className={clsx('mt-1 text-sm text-gray-500', buttonOrLink && 'mb-4')}>
+      <p className={clsx("mt-1 text-sm text-gray-500", buttonOrLink && "mb-4")}>
         {subtitle}
       </p>
     )}
