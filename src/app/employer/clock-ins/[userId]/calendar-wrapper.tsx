0d60a84c41b2ffc1685cd63221e0ae6ef6a182b7
Gabriel Fernandes <bielfer18@gmail.com>
2023-08-18 20:46:36 -0300

    feat: allow changing displaying month

--- src/app/employer/clock-ins/[userId]/calendar-wrapper.tsx
+++ src/app/employer/clock-ins/[userId]/calendar-wrapper.tsx
@@ -1,21 +1,31 @@
 "use client";
-import type { FC } from "react";
+import { useState, type FC } from "react";
 import { trpc } from "~/services/trpc";
 import Calendar from "./calendar";
 import { generateMonth } from "~/helpers/dates";
-import { isSameDay } from "date-fns";
+import { addMonths, endOfMonth, isSameDay, startOfMonth } from "date-fns";
 import LoadingWrapper from "~/components/loading-wrapper";
+import Modal from "~/components/modal";
+import FormClockIn from "~/components/forms/clock-in";
 
 type Props = {
   userId: string;
 };
 
 const CalendarWrapper: FC<Props> = ({ userId }) => {
-  const { data: clockIns, isLoading } = trpc.clockIns.getMany.useQuery({
+  const [selectedDate, setSelectedDate] = useState<Date>(new Date(2023, 7, 29));
+  const [isFormClockInOpen, setIsFormClockInOpen] = useState(false);
+  const {
+    data: clockIns,
+    isLoading,
+    refetch,
+  } = trpc.clockIns.getMany.useQuery({
     userId,
+    startDate: startOfMonth(selectedDate),
+    endDate: endOfMonth(selectedDate),
   });
 
-  const days = generateMonth(new Date()).map((day) => ({
+  const days = generateMonth(selectedDate).map((day) => ({
     time: day,
     events:
       clockIns
@@ -26,10 +36,42 @@ const CalendarWrapper: FC<Props> = ({ userId }) => {
         })) ?? [],
   }));
 
+  const goToNextMonth = () => {
+    setSelectedDate((prev) => addMonths(prev, 1));
+  };
+
+  const goToPreviousMonth = () => {
+    setSelectedDate((prev) => addMonths(prev, -1));
+  };
+
   return (
-    <LoadingWrapper className="py-10" isLoading={isLoading}>
-      <Calendar days={days} />
-    </LoadingWrapper>
+    <>
+      <LoadingWrapper className="py-10" isLoading={isLoading}>
+        <Calendar
+          date={selectedDate}
+          days={days}
+          onDayClick={(day) => {
+            setIsFormClockInOpen(true);
+            setSelectedDate(day);
+          }}
+          goToNextMonth={goToNextMonth}
+          goToPreviousMonth={goToPreviousMonth}
+        />
+      </LoadingWrapper>
+      <Modal
+        isOpen={isFormClockInOpen}
+        onClose={() => setIsFormClockInOpen(false)}
+      >
+        <FormClockIn
+          className="max-w-sm"
+          date={selectedDate}
+          afterSubmit={() => {
+            setIsFormClockInOpen(false);
+            refetch();
+          }}
+        />
+      </Modal>
+    </>
   );
 };
 
--- src/app/employer/clock-ins/[userId]/calendar.tsx
+++ src/app/employer/clock-ins/[userId]/calendar.tsx
@@ -14,11 +14,19 @@ import { dayAbbreviations, monthNames } from "~/constants/dates";
 
 type Props = {
   days: { time: Date; events: { name: string; time: Date }[] }[];
+  onDayClick?: (day: Date) => void;
+  goToNextMonth: () => void;
+  goToPreviousMonth: () => void;
+  date: Date;
 };
 
-const Calendar: FC<Props> = ({ days }) => {
-  const today = new Date();
-
+const Calendar: FC<Props> = ({
+  days,
+  onDayClick,
+  goToNextMonth,
+  goToPreviousMonth,
+  date,
+}) => {
   return (
     <div className="lg:flex lg:h-full lg:flex-col">
       <header className="flex items-center justify-between py-4 lg:flex-none">
@@ -31,16 +39,18 @@ const Calendar: FC<Props> = ({ days }) => {
             <button
               type="button"
               className="flex items-center justify-center rounded-l-md py-2 pl-3 pr-4 text-gray-400 hover:text-gray-500 focus:relative md:w-9 md:px-2 md:hover:bg-gray-50"
+              onClick={goToPreviousMonth}
             >
               <span className="sr-only">Mês Anterior</span>
               <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
             </button>
             <div className="flex items-center justify-center px-3.5 text-sm font-semibold text-gray-900 focus:relative">
-              {monthNames[getMonth(today)]} {getYear(today)}
+              {monthNames[getMonth(date)]} {getYear(date)}
             </div>
             <button
               type="button"
               className="flex items-center justify-center rounded-r-md py-2 pl-4 pr-3 text-gray-400 hover:text-gray-500 focus:relative md:w-9 md:px-2 md:hover:bg-gray-50"
+              onClick={goToNextMonth}
             >
               <span className="sr-only">Próximo Mês</span>
               <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
@@ -60,17 +70,21 @@ const Calendar: FC<Props> = ({ days }) => {
           <div className="hidden w-full lg:grid lg:grid-cols-7 lg:gap-px">
             {days.map((day) => {
               const isToday = getIsToday(day.time);
-              const isCurrentMonth = isSameMonth(day.time, today);
+              const isCurrentMonth = isSameMonth(day.time, date);
 
               return (
-                <div
+                <button
                   key={day.time.toDateString()}
                   className={cn(
                     isCurrentMonth
                       ? "cursor-pointer bg-white hover:bg-gray-100"
-                      : "bg-gray-50 text-gray-500",
-                    "relative px-3 py-2"
+                      : "cursor-default bg-gray-50 text-gray-500",
+                    "relative flex flex-col px-3 py-2 text-left"
                   )}
+                  onClick={() => {
+                    if (!isCurrentMonth) return;
+                    if (onDayClick) onDayClick(day.time);
+                  }}
                 >
                   <time
                     dateTime={day.time.toDateString()}
@@ -108,27 +122,33 @@ const Calendar: FC<Props> = ({ days }) => {
                   ) : (
                     <div className="h-10">-</div>
                   )}
-                </div>
+                </button>
               );
             })}
           </div>
           <div className="isolate grid w-full grid-cols-7 gap-px lg:hidden">
             {days.map((day) => {
               const isToday = getIsToday(day.time);
-              const isCurrentMonth = isSameMonth(day.time, today);
+              const isCurrentMonth = isSameMonth(day.time, date);
 
               return (
                 <button
                   key={day.time.toDateString()}
                   type="button"
                   className={cn(
-                    isCurrentMonth ? "bg-white" : "bg-gray-50",
+                    isCurrentMonth
+                      ? "bg-white"
+                      : "pointer-events-none bg-gray-50",
                     isToday && "font-semibold",
                     isToday && "text-primary-600",
                     isCurrentMonth && !isToday && "text-gray-900",
                     !isCurrentMonth && !isToday && "text-gray-500",
                     "flex h-14 flex-col px-3 py-2 hover:bg-gray-100 focus:z-10"
                   )}
+                  onClick={() => {
+                    if (!isCurrentMonth) return;
+                    if (onDayClick) onDayClick(day.time);
+                  }}
                 >
                   <time
                     dateTime={day.time.toDateString()}
