644656fd24ec0f0f48b5ad0c23405a2813154428
Gabriel Fernandes <bielfer18@gmail.com>
2023-08-18 18:29:54 -0300

    feat: formik number component

---
+++ src/components/forms/formik-number/formik-number.tsx
@@ -0,0 +1,53 @@
+import { useField } from "formik";
+import { type ChangeEvent, useState } from "react";
+import Input from "../../input";
+
+interface Props {
+  label?: string;
+  name: string;
+  hint?: string;
+  format?: "default" | "currency";
+  placeholder?: string;
+  className?: string;
+  disabled?: boolean;
+}
+
+const FormikNumber = ({
+  name,
+  label,
+  hint,
+  className,
+  format = "default",
+  placeholder,
+  disabled,
+}: Props) => {
+  const [, { error, initialValue, touched }, { setValue }] = useField(name);
+  const [input, setInput] = useState(initialValue);
+
+  const formats = {
+    default: (val: string) => Number(val.replace(",", ".")),
+    currency: (val: string) => Math.floor(Number(val.replace(",", ".")) * 100),
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+
+    setInput(value);
+    setValue(formats[format](value));
+  };
+
+  return (
+    <Input
+      value={input}
+      onChange={handleChange}
+      label={label}
+      className={className}
+      error={touched && error ? error : ""}
+      hint={hint}
+      placeholder={placeholder}
+      disabled={disabled}
+    />
+  );
+};
+
+export default FormikNumber;
