38f5c4b2092ee5e53baf18f40e1ff611d96bf784
Gabriel Fernandes <bielfer18@gmail.com>
2023-08-22 16:34:53 -0300

    feat: create days off and hide employee in day off

--- src/app/employer/schedule/calendar-wrapper.tsx
+++ src/app/employer/schedule/calendar-wrapper.tsx
@@ -2,7 +2,14 @@
 import { useState, type FC, useMemo } from "react";
 import { trpc } from "~/services/trpc";
 import { generateMonth } from "~/helpers/dates";
-import { addMonths, format, getDay } from "date-fns";
+import {
+  addMonths,
+  endOfMonth,
+  format,
+  getDay,
+  isSameDay,
+  startOfMonth,
+} from "date-fns";
 import LoadingWrapper from "~/components/loading-wrapper";
 import Modal from "~/components/modal";
 import CalendarMonthView from "~/components/calendar-month-view";
@@ -12,12 +19,24 @@ const CalendarWrapper: FC = () => {
   const [selectedDate, setSelectedDate] = useState<Date>(new Date());
   const [isDaysOffOpen, setIsDaysOffOpen] = useState(false);
   const { data: schedules, isLoading } = trpc.schedules.getMany.useQuery({});
+  const { data: daysOff, refetch: refetchDaysOff } =
+    trpc.dayOff.getMany.useQuery({
+      startDate: startOfMonth(selectedDate),
+      endDate: endOfMonth(selectedDate),
+    });
+
+  console.log(daysOff);
 
   const days = useMemo(
     () =>
       generateMonth(selectedDate).map((day) => {
-        const filteredSchedules = schedules?.filter((schedule) =>
-          schedule.days.includes(getDay(day))
+        const filteredSchedules = schedules?.filter(
+          (schedule) =>
+            schedule.days.includes(getDay(day)) &&
+            !daysOff?.find(
+              (dayOff) =>
+                isSameDay(dayOff.date, day) && dayOff.userId === schedule.userId
+            )
         );
         const events: { name: string; time: string }[] = [];
         let amountOfTimes = 0;
@@ -49,7 +68,7 @@ const CalendarWrapper: FC = () => {
           events,
         };
       }),
-    [selectedDate, schedules]
+    [selectedDate, schedules, daysOff]
   );
 
   const goToNextMonth = () => {
@@ -113,7 +132,11 @@ const CalendarWrapper: FC = () => {
         className="w-full max-w-md"
       >
         <h3>Escala em {format(selectedDate, "dd/MM")}</h3>
-        <EmployeesAvailable employeeSchedules={filteredSchedulesByDay} />
+        <EmployeesAvailable
+          date={selectedDate}
+          employeeSchedules={filteredSchedulesByDay}
+          refetchDaysOff={() => refetchDaysOff()}
+        />
       </Modal>
     </>
   );
--- src/app/employer/schedule/employees-available.tsx
+++ src/app/employer/schedule/employees-available.tsx
@@ -1,8 +1,15 @@
+import { endOfDay, startOfDay } from "date-fns";
 import type { FC } from "react";
 import Button from "~/components/button";
+import LoadingWrapper from "~/components/loading-wrapper";
 import StackedList from "~/components/stacked-list";
+import { useToast } from "~/components/toast";
+import { tryCatch } from "~/helpers/try-catch";
+import { trpc } from "~/services/trpc";
 
 type Props = {
+  date: Date;
+  refetchDaysOff: () => void;
   employeeSchedules: {
     employeeId: string;
     employeeName: string;
@@ -11,21 +18,96 @@ type Props = {
   }[];
 };
 
-const EmployeesAvailable: FC<Props> = ({ employeeSchedules }) => {
+const EmployeesAvailable: FC<Props> = ({
+  employeeSchedules,
+  date,
+  refetchDaysOff,
+}) => {
+  const { addToast } = useToast();
+  const {
+    data: daysOff,
+    isLoading,
+    refetch,
+  } = trpc.dayOff.getMany.useQuery({
+    startDate: startOfDay(date),
+    endDate: endOfDay(date),
+  });
+  const { mutateAsync: createDayOff, isLoading: isCreatingDayOff } =
+    trpc.dayOff.create.useMutation();
+  const { mutateAsync: deleteDayOff, isLoading: isDeletingDayOff } =
+    trpc.dayOff.delete.useMutation();
+
   return (
-    <StackedList
-      items={employeeSchedules.map((item) => ({
-        id: item.employeeId,
-        imageUrl: item.employeeImageUrl,
-        name: item.employeeName,
-        subName: item.schedule,
-        onRight: (
-          <Button variant="secondary" size="xs">
-            Dar Folga
-          </Button>
-        ),
-      }))}
-    />
+    <LoadingWrapper isLoading={isLoading}>
+      <StackedList
+        items={employeeSchedules.map((employeeSchedule) => ({
+          id: employeeSchedule.employeeId,
+          imageUrl: employeeSchedule.employeeImageUrl,
+          name: employeeSchedule.employeeName,
+          subName: employeeSchedule.schedule,
+          onRight: !daysOff?.find(
+            (dayOff) => dayOff.userId === employeeSchedule.employeeId
+          ) ? (
+            <Button
+              variant="secondary"
+              size="xs"
+              loading={isCreatingDayOff}
+              disabled={isCreatingDayOff}
+              onClick={async () => {
+                const [, error] = await tryCatch(
+                  createDayOff({
+                    date,
+                    userId: employeeSchedule.employeeId,
+                  })
+                );
+                if (error) {
+                  addToast({
+                    type: "error",
+                    content:
+                      "Falha ao dar folga, tente novamente ao sumir essa mensagem",
+                    duration: 5000,
+                  });
+                  return;
+                }
+
+                refetch();
+                refetchDaysOff();
+              }}
+            >
+              Dar Folga
+            </Button>
+          ) : (
+            <Button
+              variant="danger"
+              size="xs"
+              loading={isDeletingDayOff}
+              disabled={isDeletingDayOff}
+              onClick={async () => {
+                const id = daysOff.find(
+                  (dayOff) => dayOff.userId === employeeSchedule.employeeId
+                )?.id as number;
+                const [, error] = await tryCatch(deleteDayOff({ id }));
+
+                if (error) {
+                  addToast({
+                    type: "error",
+                    content:
+                      "Falha ao retirar folga, tente novamente ao sumir essa mensagem",
+                    duration: 5000,
+                  });
+                  return;
+                }
+
+                refetch();
+                refetchDaysOff();
+              }}
+            >
+              Remover Folga
+            </Button>
+          ),
+        }))}
+      />
+    </LoadingWrapper>
   );
 };
 
