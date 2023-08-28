a735069613b806d7dc5cf270b61f4ffd2fa1dfa8
Gabriel Fernandes <bielfer18@gmail.com>
2023-08-28 14:24:45 -0300

    feat: if user has logged in before, redirect accordingly

--- src/app/choose-role/page.tsx
+++ src/app/choose-role/page.tsx
@@ -1,7 +1,10 @@
+import { auth } from "@clerk/nextjs";
 import { type Metadata } from "next";
+import { redirect } from "next/navigation";
 import { type FC } from "react";
 import MyLink from "~/components/my-link";
 import { paths } from "~/constants/paths";
+import { roles } from "~/constants/roles";
 
 export const metadata: Metadata = {
   title: "Escolha seu papel | ZipRH",
@@ -9,6 +12,11 @@ export const metadata: Metadata = {
 };
 
 const ChooseRolePage: FC = () => {
+  const { orgRole } = auth();
+
+  if (orgRole === roles.admin) return redirect(paths.employerSchedule);
+  if (orgRole === roles.basicMember) return redirect(paths.employeeSchedule);
+
   return (
     <div className="flex h-screen items-center justify-center">
       <div className="flex flex-col items-center justify-center gap-y-6 rounded-xl border p-10 shadow">
