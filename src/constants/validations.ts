0ab4dec6139b7442a466836d326fca257ff16076
Gabriel Fernandes <bielfer18@gmail.com>
2023-08-29 16:08:45 -0300

    feat: number and calendar validations

--- src/constants/validations.ts
+++ src/constants/validations.ts
@@ -1,5 +1,8 @@
 export const validations = {
   required: "Campo Obrigatório",
+  number: "Insira apenas números",
+  calendar: "Use o calendário para escolher a data",
+  valueGreaterThan: (num: number) => `Valor deve ser maior que ${num}`,
 };
 
 export const hints = {
