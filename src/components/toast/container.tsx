f8639d14d7bb36316884296f1932e5bdfdb251e8
Gabriel Fernandes <bielfer18@gmail.com>
2023-09-01 13:49:26 -0300

    fix: toast fixed on top

--- src/components/toast/container.tsx
+++ src/components/toast/container.tsx
@@ -1,14 +1,14 @@
-import { Transition } from '@headlessui/react';
-import { useToastStore } from '~/store';
-import Toast from './toast';
-import { type FC } from 'react';
+import { Transition } from "@headlessui/react";
+import { useToastStore } from "~/store";
+import Toast from "./toast";
+import { type FC } from "react";
 
 const ToastContainer: FC = () => {
   const { toasts } = useToastStore();
 
   return (
     <Transition appear show={toasts.length > 0}>
-      <div className="absolute bottom-5 right-5 flex w-5/6 max-w-sm flex-col gap-y-3 overflow-hidden">
+      <div className="fixed right-5 top-5 flex w-5/6 max-w-sm flex-col gap-y-3 overflow-hidden">
         {toasts.map((item) => (
           <Toast key={item.id} {...item} />
         ))}
