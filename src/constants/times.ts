8e33027eb72ed343cafc27a81f22b8c1358c320e
Gabriel Fernandes <bielfer18@gmail.com>
2023-09-02 13:41:36 -0300

    fix: change minutes interval to 5

--- src/constants/times.ts
+++ src/constants/times.ts
@@ -1,9 +1,14 @@
 export const times = (() => {
   const times: string[] = [];
+  const interval = 5;
 
   for (let i = 0; i < 24; i++) {
-    for (let j = 0; j < 4; j++) {
-      times.push(`${i}:${15 * j === 0 ? "00" : 15 * j}`);
+    for (let j = 0; j < 60 / interval; j++) {
+      times.push(
+        `${i}:${
+          interval * j === 0 ? "00" : interval * j === 5 ? "05" : interval * j
+        }`
+      );
     }
   }
 
